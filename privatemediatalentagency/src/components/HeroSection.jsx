c4c04e2f8ef72f35269d9cbc1fe2863fea9bf44c
MojolaoluwaGafar <mojolaoluwa1212@gmail.com>
2025-03-20 20:03:18 +0100

    fixes

--- privatemediatalentagency/src/components/HeroSection.jsx
+++ privatemediatalentagency/src/components/HeroSection.jsx
@@ -1,21 +1,16 @@
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
 
 export default function HeroSection() {
-    return (
-      <div className="bg-[url(https://live.staticflickr.com/8219/8410945580_8516801903_z.jpg)] bg-cover bg-center h-[80vh] pt-[20%]">
-        <div className="pt-[50%] w-full lg:w-[500px] mx-auto">
-          <h1 className="text-center font-bold text-white text-2xl lg:text-5xl">
-            Adult Talent Agency <br /> for Pornstars, Influencers, and Models
-          </h1>
-          <Link
-            className="flex justify-center items center pt-3 text-md"
-            to="/application"
-          >
-            <button className="bg-teal-400 hover:bg-teal-900 py-2 px-4 text-white rounded-sm ">
-              Apply Now
-            </button>
-          </Link>
-        </div>
-      </div>
-    );
+  return (
+    <div className="bg-[url(https://live.staticflickr.com/8219/8410945580_8516801903_z.jpg)] bg-cover bg-center h-[80vh] flex flex-col justify-center items-center text-center px-4">
+      <h1 className="font-bold text-white text-2xl lg:text-5xl">
+        Adult Talent Agency <br /> for Pornstars, Influencers, and Models
+      </h1>
+      <Link to="/application" className="mt-4">
+        <button className="bg-teal-400 hover:bg-teal-900 py-2 px-4 text-white rounded-sm">
+          Apply Now
+        </button>
+      </Link>
+    </div>
+  );
 }
